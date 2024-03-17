6109f6d08ebe872dd1472f6e6c35469226591aa8
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-03-17 10:45:14 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // DEPENDENCIES
 const express = require('express')
+const { Sequelize } = require('sequelize')
 const app = express()
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// DATABASE
+const sequelize = new Sequelize(process.env.postgress);
+try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 // ROOT
 app.get('/', (req, res) => {
