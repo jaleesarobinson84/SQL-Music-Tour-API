397ad076fed936153d5550a3930d629780d1d695
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-03-17 13:42:30 -0400

    migration install

---
+++ config/config.js
@@ -0,0 +1,23 @@
+{
+  "development": {
+    "username": "root",
+    "password": "Begreat24!",
+    "database": "music_tour",
+    "host": "127.0.0.1",
+    "dialect": "postgress"
+  },
+  "test": {
+    "username": "root",
+    "password": "Begreat24!",
+    "database": "music_tour_test",
+    "host": "127.0.0.1",
+    "dialect": "postgress"
+  },
+  "production": {
+    "username": "root",
+    "password": "Begreat24!",
+    "database": "music_tour_production",
+    "host": "127.0.0.1",
+    "dialect": "postgress"
+  }
+}
--- server.js
+++ server.js
@@ -9,13 +9,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // DATABASE
-const sequelize = new Sequelize(process.env.postgress);
-try {
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+ if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 
 // ROOT
 app.get('/', (req, res) => {
