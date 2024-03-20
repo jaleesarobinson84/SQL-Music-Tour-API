210f479d096c36fa825c9bd270fa00ad1daf3a68
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-03-20 19:16:14 -0400

    event and stage controllers add

---
+++ controllers/event.js
@@ -0,0 +1,12 @@
+const { Op } = require('sequelize')
+const events = require('express').Router()
+const db = require('../models')
+const { Event } = db
+
+
+
+
+
+
+
+module.exports = events
---
+++ controllers/stage.js
@@ -0,0 +1,9 @@
+const { Op } = require('sequelize')
+const stages = require('express').Router()
+const db = require('../models')
+const { Stage } = db
+
+
+
+
+module.exports = stages
